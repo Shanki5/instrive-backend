3266432f83475e06e077164bf58a96c52a064b16
Shankrith S <shankrith1618+github@gmail.com>
2023-02-02 15:12:04 +0530

    configure basic server

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const routes = require("./routes");
 const mongoose = require("mongoose");
-var multer = require("multer");
-var upload = multer();
+const multer = require("multer");
+const model = require("./model/model");
+const { required } = require("nodemon/lib/config");
+const upload = multer({ dest: "uploads/" });
+const readXlsxFile = require("read-excel-file/node");
 
 require("dotenv").config();
+
+const schema = {
+    College: {
+        prop: "College",
+        type: String,
+    },
+    University: {
+        prop: "University",
+        type: String,
+    },
+};
 
 const mongoString = process.env.DATABASE_URL;
 
@@ -21,11 +34,33 @@ database.once("connected", () => {
 
 const app = express();
 
-// app.use(express.json());
-app.use(upload.array());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", routes);
+const router = express.Router();
 
+router.post("/post", upload.single("file"), async (req, res) => {
+    readXlsxFile(req.file.path, { schema }).then(({ rows, errors }) => {
+        const validatedRows = rows.filter((value) => {
+            return value.College && value.University;
+        });
+        const data = new model({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            message: req.body.message,
+            file: validatedRows,
+        });
+        try {
+            const dataToSave = data.save();
+            res.status(200).json(dataToSave);
+        } catch (error) {
+            res.status(400).json({ message: error.message });
+        }
+    });
+});
+
+app.use("/api", router);
 app.listen(8080, () => {
     console.log("Server started!!");
 });
